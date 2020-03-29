d86b3465a39645ce8f34667796575bf97871455b
Arseniy Obolenskiy <gooddoog@student.su>
2020-03-29 23:06:10 +0300

    Fill up emoticons category

--- src/emojikeyboard.cpp
+++ src/emojikeyboard.cpp
@@ -9,9 +9,14 @@ EmojiKeyboard::EmojiKeyboard() :
     QString str = "";
 
     QVector <Emoji*> emojis;
-    for(unsigned char i = 0x81; i < 0xBF; ++i) {
+    for(unsigned char i = 0x81; i < 0xC0; ++i) {
         char a[5] = {};
         snprintf(a, 5, "\xF0\x9F\x98%c", i);
+        emojis.push_back(new Emoji(a));
+    }
+    for(unsigned char i = 0x81; i < 0x90; ++i) {
+        char a[5] = {};
+        snprintf(a, 5, "\xF0\x9F\x99%c", i);
         emojis.push_back(new Emoji(a));
     }
     for (int r = 0; r < (emojis.size() + 15) / 16; ++r) {
