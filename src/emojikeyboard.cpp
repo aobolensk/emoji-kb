771d22e638f81c3f9b4811794786f8d49df9e416
Arseniy Obolenskiy <gooddoog@student.su>
2020-03-29 23:28:58 +0300

    Add miscellaneous emojis

--- src/emojikeyboard.cpp
+++ src/emojikeyboard.cpp
@@ -19,6 +19,20 @@ EmojiKeyboard::EmojiKeyboard() :
         snprintf(a, 5, "\xF0\x9F\x99%c", i);
         emojis.push_back(new Emoji(a));
     }
+    for(unsigned char i = 0x9A; i < 0x9B; ++i) {
+        for(unsigned char j = 0x81; j < 0xC0; ++j) {
+            char a[5] = {};
+            snprintf(a, 5, "\xF0\x9F%c%c", i, j);
+            emojis.push_back(new Emoji(a));
+        }
+    }
+    for(unsigned char i = 0x8C; i < 0x8F; ++i) {
+        for(unsigned char j = 0x81; j < 0xC0; ++j) {
+            char a[5] = {};
+            snprintf(a, 5, "\xF0\x9F%c%c", i, j);
+            emojis.push_back(new Emoji(a));
+        }
+    }
     for (int r = 0; r < (emojis.size() + 15) / 16; ++r) {
         QHBoxLayout *row = new QHBoxLayout(nullptr);
         for (int i = r * 16; i < qMin(emojis.size(), (r + 1) * 16); ++i) {
